32b4b6f88401b0d116b9df48156aaf664574783f
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-25 00:10:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,17 @@ end
 
 def monopoly_with_third_tier
 		monopoly = {
-  	  :railroads => {:pieces => 4, :name =>{}, :rent_in_dollars => {} }
+  	  :railroads => {
+  	    :pieces => 4,
+  	    :name =>{}, 
+  	    :rent_in_dollars => {
+  	      :one_piece_owned => "$25" 
+  	      :two_piece_owned => "$50"
+  	      :three_piece_owned => "$100"
+  	      :four_piece_owned => "$200"
+  	  } 
+  	    
+  	  }
   	}
 
 end
